72fef37d1d3a6e0d1d45eefd10d74e4d44358a20
crazyCode2 <1178770858@qq.com>
2018-09-16 19:35:53 +0800

    添加 iview

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -14,6 +14,11 @@
       </div>
     </div>
 
+    <i-card full title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+      <view slot="content">内容不错</view>
+      <view slot="footer">尾部内容</view>
+    </i-card>
+
     <form class="form-container">
       <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
       <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -3,3 +3,11 @@ import App from './index'
 
 const app = new Vue(App)
 app.$mount()
+
+export default {
+  config: {
+    usingComponents: {
+      'i-card': '../../../static/iview/card/index'
+    }
+  }
+}
