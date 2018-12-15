d3800427d41fac4ff158b2608b1690161e7d5199
crazyCode2 <1178770858@qq.com>
2018-12-15 23:11:40 +0800

    完成头部商户信息

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -1,13 +1,36 @@
 import Taro, { Component } from '@tarojs/taro';
-import { View, Text } from '@tarojs/components';
+import { View, Text, Image } from '@tarojs/components';
 import Top from './top';
+import './head.scss';
 
 class Head extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      store: {
+        title:"川湘居",
+        notice:"欢迎光临，很高兴为您服务~",
+        tags:["味道赞","主食真好","分量足"]
+      }
+    };
+  }
+
   render () {
+    let { store } = this.state;
     return (
-      <View>
+      <View className="head">
         <Top />
-        <Text>整体的头部</Text>
+        <Image className="back" src={require('../../assets/img/back.jpg')} />
+        <View className="store">
+          <Image className="store_img" src={require('../../assets/img/store.jpg')} />
+          <View className="store_text">
+            <Text>{store.title}</Text>
+            <Text>{store.notice}</Text>
+            <View>
+              {store.tags.map((item,index) => <Text className="tags_text" key={index}>{item}</Text>)}
+            </View>
+          </View>
+        </View>
       </View>
     )
   }
