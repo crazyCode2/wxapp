277c9cd51055443e62d54852779f835313b3ef0c
crazyCode2 <1178770858@qq.com>
2018-12-16 18:41:24 +0800

    完成底部样式

--- src/components/bottom/bottom.js
+++ src/components/bottom/bottom.js
@@ -9,21 +9,26 @@ class Bottom extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      //
+      Num: 0,
+      sendPrice: 5, // 配送费
+      supportTakeBySelf: false,
+      sendMustPrice:20 // 满多少钱起送
     };
   }
 
   render () {
+    let { Num, sendPrice, supportTakeBySelf, sendMustPrice } = this.state;
     return (
       <View className="bottom">
         <View className="bottom_body">
+          {Num ? <Text className="num">{this.state.Num}</Text> : null}
           <Image className="store_img" src={require('../../assets/img/emptystore.png')} />
           <View className="info">
-            <Text>另需配送费￥3</Text>
-            <Text>支持自取</Text>
+            <Text>{sendPrice ? `另需配送费￥${sendPrice} | `:""}</Text>
+            <Text>{supportTakeBySelf ? "支持自取":"不支持自取"}</Text>
           </View>
           <View className="submit">
-            <Text>￥20起送</Text>
+            <Text>{sendMustPrice ? `￥${sendMustPrice}起送`:""}</Text>
           </View>
         </View>
       </View>
