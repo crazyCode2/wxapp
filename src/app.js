6763bb425ab72287be2b0deb5bed718117ce6134
crazyCode2 <1178770858@qq.com>
2018-12-23 13:03:48 +0800

    项目初始化

--- src/app.js
+++ src/app.js
@@ -1,66 +1,35 @@
-import '@tarojs/async-await'
 import Taro, { Component } from '@tarojs/taro'
-import Home from './pages/home'
-import dva from './utils/dva'
-import models from './models'
-import { Provider } from '@tarojs/redux'
+import Index from './pages/index'
 
-import './styles/base.scss'
-
-
-const dvaApp = dva.createApp({
-  initialState: {},
-  models: models,
-});
-const store = dvaApp.getStore();
+import './app.less'
 
 class App extends Component {
 
   config = {
     pages: [
-      'pages/home/index',
-      'pages/cart/index',
-      'pages/user/index',
+      'pages/index/index'
     ],
     window: {
-      backgroundTextStyle: 'dark',
+      backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: '女装租赁平台',
+      navigationBarTitleText: 'WeChat',
       navigationBarTextStyle: 'black'
-    },
-    tabBar: {
-      list: [{
-        pagePath: "pages/home/index",
-        text: "首页",
-        iconPath: "./images/tab/home.png",
-        selectedIconPath: "./images/tab/home-active.png"
-      }, {
-        pagePath: "pages/cart/index",
-        text: "衣袋",
-        iconPath: "./images/tab/cart.png",
-        selectedIconPath: "./images/tab/cart-active.png"
-      },{
-        pagePath: "pages/user/index",
-        text: "我的",
-        iconPath: "./images/tab/user.png",
-        selectedIconPath: "./images/tab/user-active.png"
-      }],
-      color: '#333',
-      selectedColor: '#333',
-      backgroundColor: '#fff',
-      borderStyle: '#ccc'
     }
   }
 
-  componentDidMount() {
+  componentDidMount () {}
 
-  }
+  componentDidShow () {}
 
-  render() {
-    return (<Provider store={store}>
-      <Home/>
-    </Provider>);
+  componentDidHide () {}
+
+  componentCatchError () {}
+
+  render () {
+    return (
+      <Index />
+    )
   }
 }
 
-Taro.render(<App/>, document.getElementById('app'))
+Taro.render(<App />, document.getElementById('app'))
